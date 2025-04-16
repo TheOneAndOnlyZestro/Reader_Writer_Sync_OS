77ed2f35a553731088c2d4ac8719db3d82b13c3e
TheOneAndOnlyZestro <TheOneAndOnlyZestro@gmail.com>
2025-04-16 15:04:37 +0200

    imported necessary files in milestone_1_reader_writer_sync

---
+++ main_files/milestone_1_reader_writer_sync.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //For normal I/O operations
+#include <unistd.h> //For standard os operations like fork, sleep,etc
+#include <pthreads.h> //For handling Threads
+#include <semaphore.h> //For semaphores
+
+
+
